2a7e55ba196598cab5913a285c181a778fe08c8f
Wendy <hsiaw@seattleu.edu>
2021-01-30 22:04:20 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Kunai", Description="Sharp, multipurpose throwing knife.", Value=2},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Shuriken", Description="Sharp weapon made for throwing and spinning.", Value=1},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Soldier Pill", Description="Restores chakra and temporarily buffs stats.", Value=3},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Exploding Tag", Description="Sealed paper that will explode upon chakra infusion.", Value=5},
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Senbon", Description="Pointed needle meant to be thrown or stabbed with.", Value=4}
             };
         }
 
