cadf8e589516c6d755707bfa75ea419dfd51d643
Wendy <hsiaw@seattleu.edu>
2021-02-11 17:16:11 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
